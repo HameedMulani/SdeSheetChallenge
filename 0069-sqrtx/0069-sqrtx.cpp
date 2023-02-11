c4f5aa23366d6fe0eb235bcf3cb12cb2b4e83906
HameedMulani <55029779+HameedMulani@users.noreply.github.com>
2023-02-11 23:49:04 +0530

    Time: 7 ms (36.08%), Space: 6 MB (62.33%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        // y^2 = x 
-        long long y=0;
-        while(y*y <= x) y++;
+    // Binary Search
+        long long s=0, e=INT_MAX, ans=0;
         
-        return y-1;
+        while(s<=e){
+            long long mid = s+(e-s)/2;
+            if(mid*mid <= x){
+                ans = mid;
+                s = mid +1;
+            }else{
+                e = mid-1;
+            }
+        }
+        
+        return ans;
     }
 };
