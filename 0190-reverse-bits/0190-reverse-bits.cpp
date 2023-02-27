c273fef879b90b2a6cf6375da3da723721ffea50
HameedMulani <55029779+HameedMulani@users.noreply.github.com>
2023-02-27 22:45:27 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (37.09%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+   
+    uint32_t reverseBits(uint32_t n) {
+    uint32_t ans=0;
+    int d=0;
+    while(n>0){
+        ans += (n&1) <<(31-d);
+        n>>=1;
+        d++;
+    }
+    return ans;
+    }
+};
