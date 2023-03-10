667d0058848a0eb8a5ed64e8b15bb455bca73d1b
HameedMulani <55029779+HameedMulani@users.noreply.github.com>
2023-03-10 22:51:43 +0530

    Time: 29 ms (27.31%), Space: 23.2 MB (51.09%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -4,7 +4,7 @@ public:
         int n = s.size();
         int l = 0; 
         int r = n-1;
-      while(l<=r){
+      while(l<r){
           char temp = s[l];  
           s[l] = s[r];
           s[r] = temp;
